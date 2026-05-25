f53f0844039c234d4c2d164161d89f5670f67536
Vikas Sharma <vikas2002g@gmail.com>
2026-05-26 02:41:13 +0530

    fixed web scrapping

--- web scraping/scrapingzzx.py
+++ web scraping/scrapingzzx.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 url = "https://www.google.com/"
 
 response = requests.get(url)
-soup = BeautifulSoup(response., 'html.parser')
+soup = BeautifulSoup(response.text, 'html.parser')
 print ("page title :", soup.title.text)
 print ("Paragraph : ", soup.find('p').text)
 # print ("Paragraph : ", soup.prettify())
